4028196c4a382eeaa39203559dbf591d2ee3ccea
Szabolcs Tóth <tsz@purzelbaum.hu>
2022-08-04 13:21:35 +0200

    New: custom function to FontExtension.

--- Sources/WISEComponents/Extensions/FontExtension.swift
+++ Sources/WISEComponents/Extensions/FontExtension.swift
@@ -39,6 +39,11 @@ public extension UIFont.TextStyle {
 }
 
 public extension UIFont {
+    private static func custom(_ font: Inter, relativeTo style: UIFont.TextStyle) -> UIFont {
+        UIFont(name: font.rawValue, size: style.size) ?? UIFont.systemFont(ofSize: style.size)
+    }
+    
     static let heading1 = UIFont(name: Inter.bold.rawValue, size: UIFont.TextStyle.largeTitle.size)
+    static let heading2 = custom(.bold, relativeTo: .title1)
     static let subText = UIFont(name: Inter.light.rawValue, size: UIFont.TextStyle.footnote.size)
 }
