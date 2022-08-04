882227f5f0d8fa3d35e6f08ed3f59a53c340e7ac
Szabolcs Tóth <tsz@purzelbaum.hu>
2022-08-05 00:19:01 +0200

    New: alpha extenion added to UIImage.

--- Sources/WISEComponents/Extensions/ImageExtension.swift
+++ Sources/WISEComponents/Extensions/ImageExtension.swift
@@ -7,7 +7,7 @@
  IMPORTANT: You are not allowed to copy any portion of this code or redistribute with or without modifications.
  
  Created by Szabolcs Tóth (tsz@purzelbaum.hu) on 04.08.2022.
- Copyright (C) 2022 WISE Cities, LLC., https://www.wisecities.us. All Rights Reserved. 
+ Copyright (C) 2022 WISE Cities, LLC., https://www.wisecities.us. All Rights Reserved.
  
  */
 
@@ -16,4 +16,11 @@ import UIKit
 /// Custom icons from Figma
 public extension UIImage {
     static let applicationIconArrow = UIImage(named: "icon_arrow", in: .module, with: nil)
+    static let applicationIconArrowDisabled = UIImage(named: "icon_arrow", in: .module, with: nil)?.alpha(0.4)
+    
+    private func alpha(_ alpha: CGFloat) -> UIImage {
+        return UIGraphicsImageRenderer(size: size).image { _ in
+            draw(at: .zero, blendMode: .normal, alpha: alpha)
+        }
+    }
 }
